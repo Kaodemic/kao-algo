54b75c14822dd77a27a29b76945d41094c875c43
Khanh <tran_khanh@outlook.com>
2020-05-06 17:09:17 +0700

    update

---
+++ khanh/doubly-linked-list/DoublyLinkedList.js
@@ -0,0 +1,86 @@
+import * from './DoublyLinkedListNode'
+
+
+const Comparator = ()=>{};
+
+export default class DoublyLinkedList {
+    /**
+     * @param {Function} [comparatorFunction]
+     */
+
+    constructor(comparatorFunction){
+        /**
+         * @var DoublyLinkedListNode
+         */
+        this.head = null;
+        this.tail = null;
+
+        this.compare = new Comparator(comparatorfunction)
+    }
+
+    prepend(value){
+        const newNode = new DoublyLinkedListNode(value, this.head)
+        if(this.head){
+            this.head.previous = newNode
+        }
+        this.head = newNode
+
+        if(!this.tail){
+            this.tail = newNode
+        }
+
+        return this
+    }
+
+    append(value){
+        const newNode= new DoublyLinkedListNode(value, this.tail)
+        //no head => tail = head
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+            return this
+        }
+
+        this.tail.next = newNode
+        newNode.previous = this.tail
+
+        this.tail = newNode
+        return this
+    }
+
+    delete(value){
+        if(!this.head){
+            return null
+        }
+
+        let deletedNote = null
+        let currentNode = this.head
+
+        while(currentNode){
+            if(this.currentNode.value = value){
+                deletedNode = currentNode
+                if(deletedNode === this.head){
+                
+                    this.head = deletedNote.next
+
+                    if(this.head){
+                        this.head.previous = null; 
+                    }
+
+                    if(deletedNode == this.tail){
+                        this.tail = null
+                    }
+                } else if(deletedNode === this.tail){
+                    this.tail = deletedNode.previous
+                    this.tail.next = null
+                    
+                }else{
+                    //Node in middle
+                    //
+                }
+            }
+        }
+    }
+
+
+}
