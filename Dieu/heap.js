76eeb0ae32c4125504e546cd8c6f3d786c494247
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-05-08 22:57:02 +0700

    remove the first max value in heap

--- Dieu/heap.js
+++ Dieu/heap.js
@@ -25,9 +25,53 @@ class Heap {
       //  7     9   5   10 --> element
       index = parentIndx;
     }
-    console.log(this.heap);
+  }
+
+  extractMax() {
+    //         30 --> the first value and max value in the heap
+    //     20      11
+    //  7     9   5  --> last element
+    let max = this.heap[0]; // get the Max value in the heap -> 30
+    let lastElement = this.heap.pop(); // get the last element -> 5
+    this.heap[0] = lastElement; // replace the first element with the last element
+    //         5 --> the first value and max value in the heap
+    //     20      11
+    //  7     9  30  --> last element
+    this.sinkDown(0); // execute the method sinkdown(0); with the first value; 5
+    return max;
+  }
+
+  sinkDown(idx) {
+    let left = idx * 2 + 1; // get the left index
+    let right = idx * 2 + 2; // get the right index
+    let largest = idx;
+    const length = this.heap.length; // get the length of the heap
+
+    // if left child is greater than parent
+    if (left <= length && this.heap[left] > this.heap[largest]) {
+      largest = left;
+    }
+
+    // if right child is greater than parent
+    if (right <= length && this.heap[right] > this.heap[largest]) {
+      largest = right;
+    }
+
+    // swap
+    if (largest !== idx) {
+      //   [this.heap[largest], this.heap[idx]] = [
+      //     this.heap[idx],
+      //     this.heap[largest],
+      //   ];
+      let temp = this.heap[idx];
+      this.heap[idx] = this.heap[largest];
+      this.heap[largest] = temp;
+      this.sinkDown(largest);
+    }
   }
 }
 
 let h = new Heap();
 h.insert(11);
+h.extractMax();
+console.log(h);
