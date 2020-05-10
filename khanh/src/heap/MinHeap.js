511dcff38a09f924a459e760220f75a0e1181fe9
Khanh <tran_khanh@outlook.com>
2020-05-10 14:31:47 +0700

    update heap

---
+++ khanh/src/heap/MinHeap.js
@@ -0,0 +1,15 @@
+import Heap from "./Heap";
+export default class MaxHeap extends Heap {
+    /**
+     * Check if pair of heap elements is in correct order 
+     * For MinHeap the firt element must be always smaller or equal
+     * For MaxHeap the first element must be alway biger or equal
+     * @param {*} firstElement
+     * @param {*} secondElement
+     * @return {boolean}
+     */
+
+    pairIsInCorrectOrder(firstElement, secondElement) {
+        return this.compare.lessThanOrEqual(firstElement, secondElement)
+    }
+}
