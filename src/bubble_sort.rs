a595c618a6cbb834fc3712cf08dc1cb66408b8c8
trkhanh <tran_khanh@outlook.com>
2024-02-16 20:02:11 +0700

    initial bubble sort

---
+++ src/bubble_sort.rs
@@ -0,0 +1,20 @@
+// The lagest element is placed in its correct position. 
+// 6035
+// 0635
+// 0365
+// The second largest element at correct possition 
+// 0356
+// Total passed n-1
+// 0356
+// The remain lagest elemetns at their correct possitions 
+// 0356
+// Total no of comparitions n*(n-1)/2
+
+
+// What is time complexity
+// What is Auxiliary space 
+// Is that stable algo or not?
+// Is that easy to understand or not
+// is that require additional memory or not?
+// is that ok with large data sets 
+// How much is called is large datasets
