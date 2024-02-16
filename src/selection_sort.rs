7b2202abe894b09c259d89ece7c0674ee91ceedd
trkhanh <tran_khanh@outlook.com>
2024-02-16 16:24:27 +0700

    update

--- src/selection_sort.rs
+++ src/selection_sort.rs
@@ -28,11 +28,13 @@ pub fn sort(array: &mut[i32]) {
     let len = array.len();
     for i in 0..len {
         let mut min_index= i;
+       
         for j in (i+1)..len {
             if array[j] < array[min_index] {
                 min_index = j;
             }
         }
+
         array.swap(i, min_index)
     }
 }
