ff62a0f5b37b84f7731c761be520c43728a35615
trkhanh <tran_khanh@outlook.com>
2024-02-17 09:36:19 +0700

    update main with prinln before

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod insertion_sort;
 fn main() {
     // let mut numbers = vec![32,14,1,5,9,3,0];
     let mut numbers = vec![1,3,9,5,2,7];
+    println!("Before numbers: {:?}", numbers);
     insertion_sort::sort(&mut numbers);
     println!("Sorted numbers: {:?}", numbers);
 }
