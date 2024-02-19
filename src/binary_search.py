13ce0e8e6d36739976534033a9cd940a5b8bd505
trkhanh <tran_khanh@outlook.com>
2024-02-19 16:46:04 +0700

    update

---
+++ src/binary_search.py
@@ -0,0 +1,31 @@
+def bsearch(array, search):
+    if len(array) > 0:
+        l = 0
+        r = len(array)
+       
+
+        while l <= r:
+            mid = l + (r-1) // 2
+
+            if array[mid] == search:
+                return mid
+            elif array[mid] < search:
+                l = mid + 1
+            else: 
+                r = mid - 1
+    else:
+        return -1
+
+# Driver
+## Sort array first cost O(n)
+if __name__ == '__main__':
+    arr = [2,3,4,1,10,40]
+    arr.sort();
+
+    assert arr == [1,2,3,4,10,40], "The array does not match the expected values"
+
+    result = bsearch(arr, 4);
+    if result != -1:
+        print("Element is presnt at index", result)
+    else: 
+        print("Element is no presnet in array")
