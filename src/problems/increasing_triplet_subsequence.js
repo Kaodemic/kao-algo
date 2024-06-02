8e06ccc7de97639f1c035c60a4fd64f61ffbcb41
trkhanh <tran_khanh@outlook.com>
2024-06-02 11:18:22 +0700

    update

--- src/problems/increasing_triplet_subsequence.js
+++ src/problems/increasing_triplet_subsequence.js
@@ -3,5 +3,13 @@
  * @return {boolean}
  */
 var increasingTriplet = function(nums) {
-    
+    let result=false;
+    for(let i =0; i< nums.length; i++){
+        if (nums[i]<=nums[i+1] && nums[i+1]<=nums[i+2]){
+            console.log(nums[i])
+            result=true;
+        }
+    }
+    return result
 };
+
