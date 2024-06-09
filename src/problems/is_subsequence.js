42e4d4ba34faa5d1686385fc4850f6652d85c6c2
trkhanh <tran_khanh@outlook.com>
2024-06-09 10:12:47 +0700

    update

--- src/problems/is_subsequence.js
+++ src/problems/is_subsequence.js
@@ -8,4 +8,39 @@ function isSubsequence(string1, string2) {
   }
 
 
-  
+// Constraints:
+
+// 0 <= s.length <= 100
+// 0 <= t.length <= 104
+// s and t consist only of lowercase English letters.
+
+// Maximum of pointer is 100
+
+// -----
+//      -----
+//            -----
+
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+function isSubsequence(stringToFound, _stringReresouce) {
+  if(stringToFound.length == 0) return true;
+  var stringReresouce = new String(_stringReresouce);
+  var encreasedSequence = [];
+  stringToFound.split('').map(s => {
+    var lastIndex = Number(stringReresouce.lastIndexOf(s));
+    if (lastIndex) {
+      encreasedSequence.push(lastIndex);
+    } else {
+      return false;
+    }
+  })
+
+  return encreasedSequence[0]>0 && encreasedSequence.join('') ==  [...encreasedSequence].sort((a,b) => a-b).join('') && !_stringReresouce.includes(stringToFound)
+}
+
+
+console.log(isSubsequence('aaaaaa', 'bbaaaa'))
